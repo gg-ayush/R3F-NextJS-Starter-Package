88d0355c155298566e6b363aae4c0b7a42a2adb4
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-04-03 21:01:26 +0545

    added vehicle cesium component

--- app/cesium/page.jsx
+++ app/cesium/page.jsx
@@ -1,13 +1,15 @@
 import Clouds from '@/components/Cesium/Clouds'
 import Aircraft from '@/components/Cesium/Aircraft'
 import HumanAvatar from '@/components/Cesium/HumanAvatar'
+import Vehicle from '@/components/Cesium/Vehicle'
 
 export default function Cesium() {
   return (
     <>
       {/* <Clouds /> */}
       {/* <Aircraft /> */}
-      <HumanAvatar />
+      {/* <HumanAvatar /> */}
+      <Vehicle />
     </>
   )
 }
